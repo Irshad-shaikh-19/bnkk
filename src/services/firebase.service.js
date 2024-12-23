9770d1d40d5e0c8bdc487c9dd80aa211ef3032f9
Irshad-shaikh-19 <irshadshaikh7228@gmail.com>
2024-12-23 17:38:42 +0530

    first commit

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,13 +1,23 @@
+require('dotenv').config(); 
+
 const admin = require("firebase-admin");
-const fs = require("fs");
-const path = require("path");
 
-// Resolve the path to the firebase_configuration.json file from the 'src' folder
-const serviceAccountPath = path.join(__dirname, "../firebase_configuration.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Handle newlines in the private key
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
-// Initialize Firebase Admin SDK
+// Initialize Firebase Admin SDK with the service account
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
