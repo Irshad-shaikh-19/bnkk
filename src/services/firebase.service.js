3328d27388cb84be1ad1ba7f7825f36452b4e277
Irshad-shaikh-19 <irshadshaikh7228@gmail.com>
2024-12-23 17:44:54 +0530

    first commit

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,4 +1,7 @@
-require('dotenv').config(); 
+require('dotenv').config(); // Load environment variables
+
+// Log the value of the private key to verify it's correctly loaded
+console.log(process.env.FIREBASE_PRIVATE_KEY);
 
 const admin = require("firebase-admin");
 
@@ -6,7 +9,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Handle newlines in the private key
+  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : '',
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -15,7 +18,6 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 };
-
 
 // Initialize Firebase Admin SDK with the service account
 admin.initializeApp({
